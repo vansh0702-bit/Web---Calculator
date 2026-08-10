a9dc9a91b19f7dc35b0696d58078ed2eb72d0f84
vansh0702-bit <vanshkathuria0702@gmail.com>
2026-08-10 06:09:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// Target display input field
 const display = document.getElementById('display');
-
-// Append numbers or operators to display
 function appendValue(input) {
     display.value += input;
 }
-
-// Clear the entire display (C button)
 function clearDisplay() {
     display.value = '';
 }
-
-// Delete the last character (DEL button)
 function deleteLast() {
     display.value = display.value.toString().slice(0, -1);
 }
-
-// Safely evaluate the mathematical expression
 function calculateResult() {
     try {
         // If string contains value, evaluate it
@@ -24,7 +15,6 @@ function calculateResult() {
             display.value = eval(display.value);
         }
     } catch (error) {
-        // Fallback display if user types bad syntax like "5++"
         display.value = 'Error';
     }
 }
